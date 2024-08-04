f198a6905413a2bd276a96a9bcb247aaf6aa6fc7
Krutika Dave <krutikadave41@gmail.com>
2024-08-04 12:12:58 +0530

    updated observer pattern

--- src/com/krutika/design/patterns/observer/TestObservable.java
+++ src/com/krutika/design/patterns/observer/TestObservable.java
@@ -3,10 +3,10 @@ package com.krutika.design.patterns.observer;
 public class TestObservable {
 
 	public static void main(String[] args) {
-		WeatherStationObservable observer = new WeatherStationObservable();
-		DisplayObserver tvDisplayObserver = new TVDisplayObserver(observer);
-		DisplayObserver mobileDisplayObserver = new MobileDisplayObserver(observer);
-		observer.setWeatherData(21, 65);
+		WeatherStationObservable observerable = new WeatherStationObservable();
+		DisplayObserver tvDisplayObserver = new TVDisplayObserver(observerable);
+		DisplayObserver mobileDisplayObserver = new MobileDisplayObserver(observerable);
+		observerable.setWeatherData(21, 65);
 		
 		
 	}
